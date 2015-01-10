7252b8d4c8e3e322937fada5a936e6f120f6a728
deadman96385 <seanhoyt963@gmail.com>
2015-01-09 22:07:15 -0500

    Liquify

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Universal device configuration for the Moto G (falcon)
 ===============================
 
-Copyright 2014 - The CyanogenMod Project
+Copyright 2015 - The CyanogenMod Project
+Copyright 2015 - The LiquidSmooth Project
---
+++ liquid.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/motorola/falcon/full_falcon.mk)
+
+# Inherit some common Liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
+
+PRODUCT_RELEASE_NAME := MOTO G
+PRODUCT_NAME := liquid_falcon
